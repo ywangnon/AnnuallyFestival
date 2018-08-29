d3e0ded86b5680737cfc8f1603ebf662978c6508
ywangnon <ywangnon123@gmail.com>
2018-08-29 22:53:03 +0900

    d

---
+++ AnuallyFestival/AnuallyFestival/Festival/EventCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventCell.swift
+//  AnuallyFestival
+//
+//  Created by Hansub Yoo on 2018. 8. 29..
+//  Copyright © 2018년 hansub yoo. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
