d67dc53bf0d8c833ee7edd12d774b853a72d0016
ywangnon <ywangnon123@gmail.com>
2018-09-01 22:21:38 +0900

    d

--- AnuallyFestival/AnuallyFestival/api.swift
+++ AnuallyFestival/AnuallyFestival/api.swift
@@ -7,3 +7,5 @@
 //
 
 import Foundation
+
+var url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?ServiceKey=oiCg9z40WmbiqKsEGTX7QtlqOR3Gl5VAd8uL%2BpcQTi4W4cmvaV00haG5iTzTkNn%2BXLYUkjsYo%2FYld0Ktz%2FFwoQ%3D%3D&keyword=%EA%B0%95%EC%9B%90&MobileOS=ETC&MobileApp=AppTest"
