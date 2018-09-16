2ae88f82e994d33cf5c347af29e20a8912caa4ea
ywangnon <ywangnon123@gmail.com>
2018-09-16 22:25:12 +0900

    a

--- AnuallyFestival/AnuallyFestival/Model/FestivalData.swift
+++ AnuallyFestival/AnuallyFestival/Model/FestivalData.swift
@@ -8,3 +8,28 @@
 
 import Foundation
 
+/********************************************
+ <item>
+ <addr1>서울특별시 송파구 양재대로 932</addr1>
+ <addr2>(가락동)</addr2>
+ <areacode>1</areacode>
+ <cat1>A02</cat1>
+ <cat2>A0207</cat2>
+ <cat3>A02070200</cat3>
+ <contentid>2509701</contentid>
+ <contenttypeid>15</contenttypeid>
+ <createdtime>20171010154704</createdtime>
+ <eventenddate>20171029</eventenddate>
+ <eventstartdate>20171021</eventstartdate>
+ <firstimage>http://tong.visitkorea.or.kr/cms/resource/00/2509700_image2_1.jpg</firstimage>
+ <firstimage2>http://tong.visitkorea.or.kr/cms/resource/00/2509700_image2_1.jpg</firstimage2>
+ <mapx>127.1109831778</mapx>
+ <mapy>37.4960925880</mapy>
+ <mlevel>6</mlevel>
+ <modifiedtime>20171013140514</modifiedtime>
+ <readcount>6817</readcount>
+ <sigungucode>18</sigungucode>
+ <tel>임대개발팀 02-3435-0237</tel>
+ <title>가락몰 초밥 페스티벌 2017</title>
+ </item>
+ ********************************************/
